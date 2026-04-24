8bfb6ec689a6d2bfa53147fbe29dabe5274963b0
Omkar <omkar29pawar@gmail.com>
2026-04-24 21:19:03 +0530

    created AddressDTO Entity

--- src/main/java/com/app/ecom/model/User.java
+++ src/main/java/com/app/ecom/model/User.java
@@ -1,18 +1,37 @@
 package com.app.ecom.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
     private UserRole role = UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
