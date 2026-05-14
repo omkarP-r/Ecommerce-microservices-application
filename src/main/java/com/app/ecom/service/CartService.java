f103f920157eed900940dddc8d8c7c47b8f2e815
Omkar <omkar29pawar@gmail.com>
2026-05-14 21:01:24 +0530

    cart service

--- src/main/java/com/app/ecom/service/CartService.java
+++ src/main/java/com/app/ecom/service/CartService.java
@@ -81,4 +81,7 @@ public class CartService {
                 .orElseGet(List::of);
     }
 
+    public void clearCart(String userId) {
+        userRepository.findById(Long.valueOf(userId)).ifPresent(cartItemRepository::deleteByUser);
+    }
 }
